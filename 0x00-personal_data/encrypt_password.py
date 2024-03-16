463c7e42a2ec4ad1f3fc1cf2b579495955ce3a1e
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-16 17:57:30 +0000

    feat: add is_valid method

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,6 +5,11 @@ encrypt passwords
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     """hash password using bcrypt"""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """compare password"""
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
