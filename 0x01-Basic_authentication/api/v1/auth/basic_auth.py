1d4f2938e877261e54bcf520e6930ab7cb84beb0
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-20 01:52:15 +0000

    feat: overload current_user

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -70,3 +70,13 @@ class BasicAuth(Auth):
             return users[0]
 
         return None
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """
+        get current user from request
+        """
+        auth_header = self.authorization_header(request)
+        base64_header = self.extract_base64_authorization_header(auth_header)
+        decoded_base64 = self.decode_base64_authorization_header(base64_header)
+        email, password = self.extract_user_credentials(decoded_base64)
+        return self.user_object_from_credentials(email, password)
