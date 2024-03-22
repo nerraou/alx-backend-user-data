f81a1dcfbb4625941e76a6ca381087570739f9df
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-22 00:55:25 +0000

    fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -43,10 +43,10 @@ def check_auth():
     if auth.require_auth(request.path, excluded_paths):
         if auth.authorization_header(request) is None:
             abort(401)
-        else:
-            setattr(request, "current_user", auth.current_user(request))
         if auth.current_user(request) is None:
             abort(403)
+        else:
+            setattr(request, "current_user", auth.current_user(request))
 
 
 @app.errorhandler(401)
