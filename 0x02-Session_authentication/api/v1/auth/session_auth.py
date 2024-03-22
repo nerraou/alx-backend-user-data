0c5a9cb8ba7f2adc0a4c118a1c060cd791ea6257
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-22 01:31:37 +0000

    create get user by session id function

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -18,3 +18,9 @@ class SessionAuth(Auth):
         id = uuid4()
         self.user_id_by_session_id[str(id)] = user_id
         return str(id)
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """returns a User ID based on a Session ID:"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
