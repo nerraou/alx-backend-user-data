f22d7ea0736c8c13bb50431603e201216479414d
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-22 01:52:44 +0000

    check

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -38,10 +38,14 @@ def check_auth():
     excluded_paths = [
         "/api/v1/status/",
         "/api/v1/unauthorized/",
-        "/api/v1/forbidden/"
+        "/api/v1/forbidden/",
+        "/api/v1/auth_session/login/"
         ]
 
-    print("OK", request.path)
+    auth_header = auth.authorization_header(request)
+    session_cookie = auth.session_cookie(request)
+    if auth_header is None and session_cookie is None:
+        abort(401)
     if auth.require_auth(request.path, excluded_paths):
         if auth.authorization_header(request) is None:
             abort(401)
