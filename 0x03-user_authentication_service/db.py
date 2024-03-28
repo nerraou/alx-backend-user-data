0b177ce3bb7dcd83b3923b8e6dbd63b28411a17e
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-28 21:21:36 +0000

    chore: remove unused imports

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,8 +5,6 @@ from typing import Dict
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
