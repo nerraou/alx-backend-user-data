c2abce299ea5856d763fc4c05bbe5b9e093d9eed
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-28 22:10:02 +0000

    feat: add destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,3 +75,8 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except Exception:
             return None
+
+    def destroy_session(self, user_id: int):
+        """destroy session
+        """
+        self._db.update_user(user_id, session_id=None)
