cd915637af57002755cce09ab65d85a47f52757c
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-28 22:03:48 +0000

    feat: add POST /sessions

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """App module
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 
@@ -29,6 +29,25 @@ def users():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """login route
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    is_valid_login = AUTH.valid_login(email, password)
+    if not is_valid_login:
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+
+    response.set_cookie("session_id", session_id)
+
+    return response
 
 
 if __name__ == "__main__":
