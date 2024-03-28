3f28c49ae4d2ab239649fcbeee21ecd606c39a98
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-28 22:16:39 +0000

    feat: add DELETE /sessions

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """App module
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -48,6 +48,21 @@ def login():
     response.set_cookie("session_id", session_id)
 
     return response
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout():
+    """logout route
+    """
+    session_id = request.cookies.get("session_id")
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+
+    return redirect("/")
 
 
 if __name__ == "__main__":
