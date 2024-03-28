73d606d73be02e6cc9a998890907bdcb202724ee
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2024-03-28 21:21:09 +0000

    feat(auth): register_user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,9 @@
 """auth module
 """
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -11,3 +14,23 @@ def _hash_password(password: str) -> bytes:
     encoded_password = password.encode("utf-8")
 
     return bcrypt.hashpw(encoded_password, salt)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """register user
+        """
+        try:
+            self._db.find_user_by(email=email)
+
+            message = "User {} already exists".format(email)
+            raise ValueError(message)
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            return self._db.add_user(email, hashed_password)
